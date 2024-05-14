13716ef4d8f671e52f1ae41121b6be2913f690be
Senanmemmedov1990 <memmedov8665341@gmail.com>
2024-05-14 20:02:40 +0400

    creted new project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWorkASP.Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWorkASP.Net.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.name = "Senan";
+            ViewBag.surname = "Memmedov";
+            ViewBag.age = 33;
+            ViewBag.country = "Mozambik";
+            ViewBag.phoneNumber = +994708665341;
+
+            return View();
+        }
+    }
+}
